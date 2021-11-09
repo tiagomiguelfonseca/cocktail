152c71f571c41274ead7b4af9b1ec638de86dc3b
tiago <tiago@studiographene.com>
2021-11-09 09:47:04 +0000

    minor change

--- src/components/BrowseLinks/index.jsx
+++ src/components/BrowseLinks/index.jsx
@@ -3,12 +3,13 @@ import { Link } from "react-router-dom";
 import { Container, CustomParagon } from "./style";
 
 export default function BrowseLinks({ title }) {
-  const abc =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","u","v","w","x","y","z"];
+  
+  const alphabet = [...Array(26).keys()].map(i => String.fromCharCode(i + 97));
 
   return (
     <Container>
       {title}
-      {abc.map((value) => {
+      {alphabet.map((value) => {
         return (
           <CustomParagon>
             <Link to={`/browse/${value}`}>{value} / </Link>
